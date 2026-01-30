a6d0e6c6a3322625ccd96bba1f3745d3091091c7
hihi0hi <dkssud3524@naver.com>
2026-01-30 16:54:37 +0900

    커밋

--- src/api/soraApi.js
+++ src/api/soraApi.js
@@ -1,6 +1,7 @@
 export const soraGet = async (payload) => {
-  const res = await axios.get(`neo4jbackend-production-f300.up.railway.app`,
-    payload
+  const res = await axios.get(
+    `http://neo4jbackend-production-f300.up.railway.app`,
+    payload,
   );
 
   return res.data;
